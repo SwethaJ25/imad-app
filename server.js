e1d332ce3297e64c4cb13d5afa7de786380bf4b5
SwethaJ25 <30415514+SwethaJ25@users.noreply.github.com>
2017-08-20 22:57:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1', function (req, res) {
+  res.sendFile('the article one is served here');
+});
+app.get('/article2', function (req, res) {
+  res.sendFile('the article two is served here');
+});
+app.get('/article3', function (req, res) {
+  res.sendFile('the article three is served here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
